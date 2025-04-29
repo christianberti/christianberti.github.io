75b54a84726085d2a52344051893b1a54cdbcdbc
Christian Berti <christiangberti@gmail.com>
2025-04-29 01:08:36 -0300

    "Arreglo de rutas de imagenes"

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "TodoList",
     subtitle: "Una App de gestión de tareas",
     description: "Este proyecto fue creado para practicar gestión de estado en React, manipulación de listas dinámicas y renderizado condicional. Permite organizar tareas de forma intuitiva mientras refuerza conceptos basicos de React.",
-    image: "/images/TodoList.png",
+    image: "./images/TodoList.png",
     techs: ["React", "CSS"],
     demoLink: "https://christianberti.github.io/TodoList",
     repoLink: "https://github.com/christianberti/TodoList"
@@ -13,25 +13,7 @@ export const projects = [
     title: "Mundo Django",
     subtitle: "Blog de noticias e informacion de Django",
     description: "Diseñada con HTML y CSS, esta Landing Page destaca por su estructura clara y diseño responsivo. El enfoque principal fue mejorar la maquetación web, aplicar principios de UX/UI y optimizar estilos para una presentación atractiva.",
-    image: "/images/MundoDjango.png",
-    techs: ["HTML", "CSS"],
-    demoLink: "https://christianberti.github.io/MundoDjango",
-    repoLink: "https://github.com/christianberti/MundoDjango"
-  },
-  {
-    title: "TodoList",
-    subtitle: "Una App de gestión de tareas",
-    description: "Este proyecto fue creado para practicar gestión de estado en React, manipulación de listas dinámicas y renderizado condicional. Permite organizar tareas de forma intuitiva mientras refuerza conceptos basicos de React.",
-    image: "/images/TodoList.png",
-    techs: ["React", "CSS"],
-    demoLink: "https://christianberti.github.io/TodoList",
-    repoLink: "https://github.com/christianberti/TodoList"
-  },
-  {
-    title: "Mundo Django",
-    subtitle: "Blog de noticias e informacion de Django",
-    description: "Diseñada con HTML y CSS, esta Landing Page destaca por su estructura clara y diseño responsivo. El enfoque principal fue mejorar la maquetación web, aplicar principios de UX/UI y optimizar estilos para una presentación atractiva.",
-    image: "/images/MundoDjango.png",
+    image: "./images/MundoDjango.png",
     techs: ["HTML", "CSS"],
     demoLink: "https://christianberti.github.io/MundoDjango",
     repoLink: "https://github.com/christianberti/MundoDjango"
